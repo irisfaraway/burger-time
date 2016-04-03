d1f5710df9bf4c73403456191ba3351c73a8e6b7
irisfaraway <iris.faraway@gmail.com>
2016-04-03 21:07:02 +0100

    fixing database explosion

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,2 @@
 class User < ActiveRecord::Base
-  validates :username, presence: true,
-                       uniqueness: { case_sensitive: false }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160403194454) do
+ActiveRecord::Schema.define(version: 20160403195357) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
