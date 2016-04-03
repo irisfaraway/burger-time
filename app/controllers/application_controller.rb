0ad15efd90963532e485bee4c076baa48c9c42b8
irisfaraway <iris.faraway@gmail.com>
2016-04-03 22:24:59 +0100

    configuring CASClient

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,15 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  # Use Single Sign-On
   before_filter CASClient::Frameworks::Rails::Filter
+  before_filter :find_or_create_user
+
+  def logout
+    CASClient::Frameworks::Rails::Filter.logout(self)
+  end
+
+  def find_or_create_user
+    User.find_or_create_user(current_user)
+  end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -7,4 +7,6 @@ Rails.application.initialize!
 # Single Sign-on
 CASClient::Frameworks::Rails::Filter.configure(
   :cas_base_url => 'https://auth.bath.ac.uk/'
+  # Disabled this so SSO isn't slammed with overenthusiastic burger-ordering
+  #:authenticate_on_every_request => true
 )
