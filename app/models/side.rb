e4f38d53d5cb8ee44612eb828bc7a3559ed163da
irisfaraway <iris.faraway@gmail.com>
2016-04-04 21:50:35 +0100

    validation for sides

--- app/models/side.rb
+++ app/models/side.rb
@@ -1,2 +1,15 @@
 class Side < ActiveRecord::Base
+
+  # Name is required, must be unique and under 30 characters
+  validates :name, presence: true,
+                   uniqueness: true,
+                   length: { maximum: 30 }
+
+  # Description must be under 100 characters
+  validates :description, length: { maximum: 100 }
+
+  # Price is required and must be more than 0
+  validates :price, presence: true,
+                    numericality: { greater_than: 0 }
+
 end
