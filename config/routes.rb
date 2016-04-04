57df3add6a82ed08e069d2f04ff099c38b13758d
irisfaraway <iris.faraway@gmail.com>
2016-04-04 21:18:14 +0100

    updated nav

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :orders
-  root 'users#index'
-  get '/logout' => 'application#logout'
   resources :users
+  root 'orders#index'
+  get '/logout' => 'application#logout'
 end
