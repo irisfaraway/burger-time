4ed2b12673137247c2674fd9d5420e1a717e6c53
irisfaraway <iris.faraway@gmail.com>
2016-04-10 17:53:36 +0100

    filling validation

--- app/models/filling.rb
+++ app/models/filling.rb
@@ -1,2 +1,10 @@
 class Filling < ActiveRecord::Base
+  # Name is required, must be unique and under 30 characters
+  validates :name, presence: true,
+                   uniqueness: true,
+                   length: { maximum: 30 }
+
+  # Price is required and must be more than 0
+  validates :price, presence: true,
+                    numericality: { greater_than: 0 }
 end
