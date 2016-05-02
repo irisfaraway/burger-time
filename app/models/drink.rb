fe165fca3961017b056c86f8fa6f193b2cc8feb4
irisfaraway <iris.faraway@gmail.com>
2016-05-02 22:56:28 +0100

    basic validation

--- app/models/drink.rb
+++ app/models/drink.rb
@@ -1,2 +1,10 @@
 class Drink < ActiveRecord::Base
+  # Name is required, must be unique and under 40 characters
+  validates :name, presence: true,
+                   uniqueness: true,
+                   length: { maximum: 40 }
+
+  # Price is required and must be more than 0
+  validates :price, presence: true,
+                    numericality: { greater_than: 0 }
 end
